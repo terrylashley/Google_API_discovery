0474cd7633ccce825ef02d80b722caa301ebd49d
terrylashley <terry.l.lashley.pro@gmail.com>
2020-01-24 09:03:47 -0500

    update script

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4ad68af4e4f15c85bd8ca8291a9dcff5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = 'AIzaSyBxri2W98z7Q6JK-l5HPpdOBDiG16CFnqU'
